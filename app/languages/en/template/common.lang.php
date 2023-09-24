bf327b4a1b2609292a9fcbef03d4c2f9ce71f4af
Mostafa Taha <mostafataha011188@gmail.com>
2023-09-25 01:03:46 +0300

    added some translates

--- app/languages/en/template/common.lang.php
+++ app/languages/en/template/common.lang.php
@@ -2,6 +2,13 @@
 $_["text_dashboard"] = "Dashboard";
 $_["text_app_manager"] = "App manager";
 $_["text_general_statistics"] = "General statistics";
-$_["text_employees"] = "Employees";
+$_["text_users"] = "Users";
 $_["text_change_lang"] = "change language";
-$_["text_log_out"] = "Logout";
+$_["text_logout"] = "Logout";
+$_["text_transaction"] = "Transactions";
+$_["text_expenses"] = "Expenses";
+$_["text_store"] = "Store";
+$_["text_clients"] = "Clients";
+$_["text_suppliers"] = "Suppliers";
+$_["text_reports"] = "Reports";
+$_["text_notifications"] = "Notifications";
