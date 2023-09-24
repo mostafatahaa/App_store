5d73cd60b007629af8db956d351e605c93995929
Mostafa Taha <mostafataha011188@gmail.com>
2023-09-25 00:59:56 +0300

    added some translantes

--- app/languages/ar/template/common.lang.php
+++ app/languages/ar/template/common.lang.php
@@ -2,6 +2,13 @@
 $_["text_dashboard"] = "اللوحة الرئيسيه";
 $_["text_app_manager"] = "مدير التطبيق";
 $_["text_general_statistics"] = "الاحصائيات العامه";
-$_["text_employees"] = "الموظفيين";
+$_["text_users"] = "المستخدمين";
 $_["text_change_lang"] = "تغيير اللغه";
-$_["text_log_out"] = "تسجيل الخروج";
+$_["text_logout"] = "تسجيل الخروج";
+$_["text_transaction"] = "المعاملات";
+$_["text_expenses"] = "المصروفات";
+$_["text_store"] = "المخزن";
+$_["text_clients"] = "العملاء";
+$_["text_suppliers"] = "الموردين";
+$_["text_reports"] = "التقارير";
+$_["text_notifications"] = "الاشعارات";
