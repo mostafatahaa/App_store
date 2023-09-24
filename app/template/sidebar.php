48b9fed30d4f5feff35bc81bf3a705b107496e53
Mostafa Taha <mostafataha011188@gmail.com>
2023-09-25 01:00:49 +0300

    complete sidebar

--- app/template/sidebar.php
+++ app/template/sidebar.php
@@ -1,24 +1,53 @@
 <div class="sidebar">
     <ul>
         <li><a href="#">
-                <span class="icon"><i class="fas fa-book"></i></span>
-                <span class="title"><?= $text_app_manager ?></span>
-            </a></li>
-        <li><a href="/employee">
-                <span class="icon"><i class="fa-solid fa-users"></i></span>
-                <span class="title"><?= $text_employees ?></span>
-            </a></li>
-        <li><a href="#">
-                <span class="icon"><i class="fa-solid fa-chart-simple"></i></span>
+                <span class="icon"><i class="fa-solid fa-chart-pie"></i></span>
                 <span class="title"><?= $text_general_statistics ?></span>
             </a></li>
+
         <li><a href="#">
-                <span class="icon"><i class="fas fa-blog"></i></span>
-                <span class="title"><?= $text_app_manager ?></span>
+                <span class="icon"><i class="fa-solid fa-money-bill-transfer"></i></span>
+                <span class="title"><?= $text_transaction ?></span>
             </a></li>
+
+        <li><a href="/employee">
+                <span class="icon"><i class="fa-solid fa-money-bill"></i></span>
+                <span class="title"><?= $text_expenses ?></span>
+            </a></li>
+
+        <li><a href="#">
+                <span class="icon"><i class="fa-solid fa-shop"></i></span>
+                <span class="title"><?= $text_store ?></span>
+            </a></li>
+
+        <li><a href="#">
+                <span class="icon"><i class="fa-solid fa-users-rectangle"></i></span>
+                <span class="title"><?= $text_clients ?></span>
+            </a></li>
+
+        <li><a href="#">
+                <span class="icon"><i class="fa-solid fa-user-group"></i></span>
+                <span class="title"><?= $text_suppliers ?></span>
+            </a></li>
+
+        <li><a href="#">
+                <span class="icon"><i class="fa-solid fa-users"></i></span>
+                <span class="title"><?= $text_users ?></span>
+            </a></li>
+        <li><a href="#">
+                <span class="icon"><i class="fa-solid fa-chart-column"></i></i></span>
+                <span class="title"><?= $text_reports ?></span>
+            </a></li>
+
+        <li><a href="#">
+                <span class="icon"><i class="fa-solid fa-bell"></i></span>
+                <span class="title"><?= $text_notifications ?></span>
+            </a></li>
+
         <li><a href="#">
                 <span class="icon"><i class="fa-solid fa-right-from-bracket"></i></span>
-                <span class="title"><?= $text_log_out ?></span>
+                <span class="title"><?= $text_logout ?></span>
             </a></li>
+
     </ul>
 </div>
