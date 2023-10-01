d52b8e173bcd2042f2bbc3a7ea40be8bf6d73fa7
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-02 02:35:13 +0300

    make some changes on session manager

--- app/lib/sessionhandler.php
+++ app/lib/sessionhandler.php
@@ -1,26 +1,23 @@
 <?php
 
-define("SESSION_SAVE_PATH", dirname(realpath(__FILE__)) . DIRECTORY_SEPARATOR . "sessions");
+namespace PHPMVC\LIB;
 
-class AppSessionHandler extends SessionHandler
+class SessionManager
 {
     private $sessionName = 'MYAPPSESSION';
-    private $sessionMaxLifetime = 0;        // this means session will end when you close your browser
+    private $sessionMaxLifetime = 0;
     private $sessionSSL = false;
-    private $sessionHTTPOnly = true;        // this means the cooke cannot be accessd through the client-side-script (javascript)
+    private $sessionHTTPOnly = true;
     private $sessionPath = "/";
-    private $sessionDomain = ".test.com";   // this mean for any subdomain
+    private $sessionDomain = ".mvcapp2.test";
     private $sessionSavePath = SESSION_SAVE_PATH;
 
-    private $sessionCipherAlgo = MCRYPT_BLOWFISH;
-    private $sessionCipherMode = MCRYPT_MODE_ECB;
-    private $sessionCipherKey = "WQAS201VXZP@221";
+    private $sessionCipherAlgo = "AES-128-ECB";
+    private $sessionCipherKey = "WYCRYPT0K3Y@2016";
 
-    /*
-When use_only_cookies is disable, php will pass the sessionID via URL
-this makes the aplication more vulnerable to session hijacking attackes
-so we make it true to make php send session IDS in cookies
-*/
+    private $timeToLive = 30; // time for session to change its id
+
+
     public function __construct()
     {
         ini_set('session.use_cookies', 1);
@@ -28,7 +25,7 @@ so we make it true to make php send session IDS in cookies
         ini_set('session.use_trans_sid', 0);
         ini_set('session.save_handler', "files");
 
-        session_name($this->sessionName); // this function used to get or set session name
+        session_name($this->sessionName);
         session_save_path($this->sessionSavePath);
         session_set_cookie_params(
             $this->sessionMaxLifetime,
@@ -37,5 +34,59 @@ so we make it true to make php send session IDS in cookies
             $this->sessionSSL,
             $this->sessionHTTPOnly
         );
+    }
+
+    // this magic method invoked when accessing inaccessible or non-existent properties
+    // we use this function so if we to call sessions like this (objectName->sessionName)
+    // if the session exists it will retunr its value and if not it will return false
+    public function __get($key)
+    {
+        return $_SESSION[$key] !== false ? $_SESSION[$key] : false;
+    }
+
+    // this magic method run when writing data that is not exits
+    // we use this magic method so if we want to set a new session
+    // we will just use (objectname->sessionKey = sessionValue)
+    public function __set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    // this magic method called when we use isset() function
+    public function __isset($key)
+    {
+        return isset($_SESSION[$key]) ? true : false;
+    }
+
+    public function start()
+    {
+        if (session_id() === "") {
+            if (session_start()) {
+                $this->setSessionStartTime();
+                $this->isSessionValidity();
+            }
+        }
+    }
+
+    private function setSessionStartTime()
+    {
+        if (!isset($this->sessionStartTime)) {
+            $this->sessionStartTime = time();
+        }
+        return true;
+    }
+
+    private function isSessionValidity()
+    {
+        if ((time() - $this->sessionStartTime) > ($this->timeToLive * 60)) {
+            $this->renewSession();
+        }
+        return true;
+    }
+
+    private function renewSession()
+    {
+        $this->sessionStartTime = time();
+        return session_regenerate_id(true);
     }
 }
