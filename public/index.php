ad4f106a80de6696ff253156c9bc7fcfdf8a7f3b
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-02 17:01:57 +0300

    make some changes

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ namespace PHPMVC;
 
 use PHPMVC\LIB\Language;
 use PHPMVC\LIB\FrontController;
+use PHPMVC\LIB\SessionManager;
 use PHPMVC\LIB\TEMPLATE\Template;
 
 if (!defined("DS")) {
@@ -14,13 +15,15 @@ require_once ".." . DS . "app" . DS . "config" . DS . "config.php";
 require_once APP_PATH . DS . "lib" . DS . "auto_load.php";
 $template_parts = require_once ".." . DS . "app" . DS . "config" . DS . "templateconfig.php";
 
-session_start();
+$session = new SessionManager();
+$session->start();
+
 if (!isset($_SESSION["lang"])) {
     $_SESSION["lang"] = APP_DEFAULT_LANGUAGE;
 }
 
-
 $template = new Template($template_parts);
 $language = new Language();
+
 $front_controller = new FrontController($template, $language);
 $front_controller->dispatch();
