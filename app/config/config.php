06c1acdf8f9e59c0c2041c4805c5ce5182d6a3d0
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-02 17:00:28 +0300

    add session save path

--- app/config/config.php
+++ app/config/config.php
@@ -17,4 +17,4 @@ defined("DATABASE_DB_NAME")         ? null : define("DATABASE_DB_NAME", "storedb
 
 defined("APP_DEFAULT_LANGUAGE")     ? null : define("APP_DEFAULT_LANGUAGE", "ar");
 
-define("SESSION_SAVE_PATH", dirname(realpath(__FILE__)) . DS . "sessions");
+define("SESSION_SAVE_PATH", APP_PATH . DS . ".." . DS . "public" . DS . "sessions");
