57d12c3155268d967a8c1ef716c99478398e6b02
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-04 00:21:40 +0300

    create test controller fort testing

---
+++ app/controllers/testcontroller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PHPMVC\Controllers;
+
+use PHPMVC\LIB\Validate;
+
+class TestController extends AbstractController
+{
+    use Validate;
+
+    public function defaultAction()
+    {
+    }
+}
