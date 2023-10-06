0b51e64f09ac9c49c0862df351d489ca8095de3f
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-06 21:59:54 +0300

    complete edit action and delete action

--- app/controllers/userscontroller.php
+++ app/controllers/userscontroller.php
@@ -7,7 +7,7 @@ use PHPMVC\LIB\InputFilter;
 use PHPMVC\LIB\Messenger;
 use PHPMVC\Models\UserModel;
 use PHPMVC\Models\UsersGroupsModel;
-
+//TODO:: Create method to check if the email already exists or not.
 class UsersController extends AbstractController
 {
     use InputFilter;
@@ -24,6 +24,12 @@ class UsersController extends AbstractController
         "phoneNumber"           => "alphaNum|maximum(15)",
         "groupId"               => "requireVal|int"
 
+    ];
+
+    private $_editActionRoles =
+    [
+        "phoneNumber"           => "alphaNum|maximum(15)",
+        "groupId"               => "requireVal|int"
     ];
 
     public function defaultAction()
@@ -58,6 +64,11 @@ class UsersController extends AbstractController
             $user->subscriptionDate = date("Y-m-d");
             $user->lastLogin = date("Y-m-d H:i:s");
             $user->status = 1;
+
+            if (UserModel::userExists($user->userName)) {
+                $this->messenger->add($this->language->get("message_user_exists"), Messenger::APP_MESSAGE_ERROR);
+                $this->redirect("/users/create");
+            }
             if ($user->save()) {
                 $this->messenger->add($this->language->get("message_create_success"));
             } else {
@@ -68,6 +79,56 @@ class UsersController extends AbstractController
         }
         $this->_view();
     }
+    public function editAction()
+    {
+        $id = $this->filter_int($this->_params[0]);
+        $user = UserModel::get_by_key($id);
+        // if someone put id that is not exists this condiction will redirect user to users page
+        if ($user === false) {
+            $this->redirect("/users");
+        }
+
+        $this->_data['user'] = $user;
+
+        $this->language->load("template.common");
+        $this->language->load("user.edit");
+        $this->language->load("user.messages");
+        $this->language->load("user.labels");
+        $this->_data["groups"] = UsersGroupsModel::get_all();
+
+        // check both request submit is exists and isValid methods return true
+        if (isset($_POST["submit"]) && $this->isValid($this->_editActionRoles, $_POST)) {
+            // edit current user
+            $user->phoneNumber = $this->filter_str($_POST["phoneNumber"]);
+            $user->groupId = $this->filter_int($_POST["groupId"]);
+            if ($user->save()) {
+                $this->messenger->add($this->language->get("message_create_success"));
+            } else {
+                //NOTE::why this codes doesn't work ?
+                $this->messenger->add($this->language->get("message_create_falied"), Messenger::APP_MESSAGE_ERROR);
+            }
+            $this->redirect("/users");
+        }
+        $this->_view();
+    }
+
+    public function deleteAction()
+    {
+        $id = $this->filter_int($this->_params[0]);
+        $user = UserModel::get_by_key($id);
+        if ($user === false) {
+            $this->redirect("/users");
+        }
+        $this->language->load("user.messages");
+
+        if ($user->delete()) {
+            $this->messenger->add($this->language->get("message_delete_success"));
+        } else {
+            $this->messenger->add($this->language->get("message_delete_failed"), Messenger::APP_MESSAGE_ERROR);
+        }
+        $this->redirect("/users");
+    }
+
     //TODO::make sure this is a AJAX Request
     // NOTE:: search for the different types of header
     public function checkUserExistsAjaxAction()
