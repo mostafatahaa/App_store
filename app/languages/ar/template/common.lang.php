d7c04c7cd5b8cc9354494bf6010720d686a2a0e1
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-07 19:18:38 +0300

    add text for access denied language

--- app/languages/ar/template/common.lang.php
+++ app/languages/ar/template/common.lang.php
@@ -20,3 +20,4 @@ $_["text_store_products_type"] = "أصناف المنتجات";
 $_["text_users_list"] = "قائمة المستخدمي";
 $_["text_users_group"] = "مجموعات المستخدمين";
 $_["text_users_privileges"] = "الصلاحيات";
+$_["text_access_denied"] = "عفوا أنت ليس لديك الصلاحية لدخول هذه الصفحه";
