91234e59c93c2929c2ac2c0fd3681cc77a1d9689
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-08 18:27:08 +0300

    add language files for product categories

--- app/languages/ar/productcategories/default.lang.php
+++ app/languages/ar/productcategories/default.lang.php
@@ -1,7 +1,6 @@
 <?php
-$_["title"] = "أقسام المنتجات";
 $_["text_header"] = "أقسام المنتجات";
-$_["text_table_category_name"] = "اسم قسم المنتجات";
+$_["text_lable_category_name"] = "اسم قسم المنتجات";
 $_["text_table_control"] = "التحكم";
 $_["text_lable_category_delete"] = "هل تود حذف القسم ؟";
 $_["text_add_category"] = "انشاء قسم منتجات جديد";
