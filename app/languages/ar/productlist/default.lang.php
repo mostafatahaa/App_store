c4c40610830c790db843985e6f88372b630b4464
Mostafa Taha <mostafataha011188@gmail.com>
2023-10-09 08:53:51 +0300

    create language files for product list page

---
+++ app/languages/ar/productlist/default.lang.php
@@ -0,0 +1,10 @@
+<?php
+$_["title"] = "المنتجات";
+$_["text_header"] = "المنتجات";
+$_["text_table_name"] = "اسم المنتج";
+$_["text_table_price"] = "السعر";
+$_["text_table_quantity"] = "الكمية";
+$_["text_table_category"] = "القسم التابع له";
+$_["text_table_control"] = "التحكم";
+$_["text_table_delete_confirm"] = "هل تود حذف المنتج ؟";
+$_["text_add_product"] = "انشاء منتج جديد";
---
+++ app/languages/ar/productlist/lables.lang.php
@@ -0,0 +1,11 @@
+<?php
+$_["text_legend"] = "بيانات المنتج";
+$_["text_table_title"] = "اسم المنتج";
+$_["text_user_categoryId"] = "القسم التابع له المنتج";
+$_["text_table_quantity"] = "الكمية المتاحه";
+$_["text_table_buyPrice"] = "سعر الشراء";
+$_["text_table_sellPrice"] = "سعر البيع";
+$_["text_lable_unit"] = "وحدة قياس المنتج";
+$_["text_lable_img"] = "صورة المنتج";
+$_["text_lable_name"] = "اسم المنتج";
+$_["text_lable_save"] = "حفظ";
